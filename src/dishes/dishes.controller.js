cc21ee3ce1c1046b9e470c396b6d347faafdff49
Albert <wholockianguitar@gmail.com>
2022-04-12 22:27:51 -0500

    initial commit

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,27 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function read(req, res) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find((dish) => dish.id === Number(dishId));
+    res.json({ data: foundDish });
+}
+function list(req, res) {
+    res.json({ data: dishes})
+}
+
+function create(req, res) {
+    const reqBody = res.locals.reqBody;
+    const newDish = {
+        ...redBody,
+        id: nextId(),
+    };
+    dishes.push(newDish);
+    res.json({ data: newDish})
+}
+
+module.exports = {
+    read,
+    list,
+    create
+}
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,13 @@
+const controller = require("./dishes.controller")
+const { get } = require("express/lib/response");
 const router = require("express").Router();
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+
+router.route("/:dishId").get(controller.read);
+
+router.route("/").get(controller.list);
+
+router.route("/").get(controller.create);
 
 module.exports = router;
