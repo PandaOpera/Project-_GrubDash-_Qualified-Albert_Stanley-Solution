f9aa2a11b4b9cad9f4ecb62399a9d49ae48433cf
Albert <wholockianguitar@gmail.com>
2022-04-16 12:40:17 -0500

    submit commit

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,13 +1,14 @@
-const controller = require("./dishes.controller")
-const { get } = require("express/lib/response");
 const router = require("express").Router();
 
-// TODO: Implement the /dishes routes needed to make the tests pass
+const controller = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/:dishId").get(controller.read);
+router
+    .route("/:dishId")
+    .get(controller.read)
+    .put(controller.update)
+    .all(methodNotAllowed);
 
-router.route("/").get(controller.list);
-
-router.route("/").get(controller.create);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
 module.exports = router;
